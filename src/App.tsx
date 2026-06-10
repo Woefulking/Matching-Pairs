eb0d699a49ca24bf8f3a6d207e73c91b7b5e3770
Woefulking <anton.ukrainchenko.a@gmail.com>
2026-06-10 22:02:10 +0300

    add card matching logic and matched cards storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import test from './assets/test.png';
+import earth from './assets/Earth.png';
 import './App.css';
 
-function App() {
-  const [uniqueCards, setUniqueCards] = useState([]);
+interface cardType {
+  id: number;
+  img: string;
+  name: string;
+  uniqueId?: string;
+}
 
-  const allCards = [
+function App() {
+  const [uniqueCards, setUniqueCards] = useState<cardType[]>([]);
+
+  const [firstSelected, setFirstSelected] = useState<cardType | null>();
+  const [secondSelected, setSecondSelected] = useState<cardType | null>();
+
+  const [matchedCards, setMatchedCards] = useState<cardType[]>([]);
+
+  const isBoardLocked = firstSelected && secondSelected;
+
+  const allCards: cardType[] = [
     { id: 1, img: '🍎', name: 'apple' },
     { id: 2, img: '🍌', name: 'banana' },
     { id: 3, img: '🍇', name: 'grape' },
@@ -14,7 +30,7 @@ function App() {
     { id: 7, img: '🍊', name: 'orange' },
   ];
 
-  const generateGameDeck = (cardsArray, count) => {
+  const generateGameDeck = (cardsArray: cardType[], count: number) => {
     const copy = [...cardsArray];
 
     for (let i = 0; i < count; i++) {
@@ -43,11 +59,58 @@ function App() {
     setUniqueCards(array);
   };
 
-  console.log(uniqueCards);
+  console.log(firstSelected);
+  console.log(secondSelected);
+  console.log(matchedCards);
+
+  const handleCardClick = (card: cardType) => {
+    if (isBoardLocked) return;
+
+    if (firstSelected) {
+      setSecondSelected(card);
+    } else {
+      setFirstSelected(card);
+    }
+  };
+
+  useEffect(() => {
+    if (isBoardLocked) {
+      if (firstSelected.name === secondSelected.name) {
+        console.log('Совпали');
+        setMatchedCards((prev) => [...prev, firstSelected, secondSelected]);
+        setFirstSelected(null);
+        setSecondSelected(null);
+      }
+
+      if (firstSelected.name !== secondSelected.name) {
+        console.log('Несовпали');
+        setFirstSelected(null);
+        setSecondSelected(null);
+      }
+    }
+  }, [firstSelected, secondSelected, isBoardLocked]);
 
   return (
     <>
-      <button onClick={() => handleStart()}>НАЖМИ НА МЕНЯ</button>
+      <button onClick={() => handleStart()} className="btn">
+        НАЖМИ НА МЕНЯ
+      </button>
+      <div className="board">
+        {uniqueCards.map((card, index) => {
+          const isMatched = matchedCards.some((matched) => matched.uniqueId === card.uniqueId);
+
+          return (
+            <button
+              type="button"
+              className={`card ${isMatched ? 'card--matched' : ''}`}
+              key={index}
+              onClick={() => handleCardClick(card)}
+            >
+              {card.img}
+            </button>
+          );
+        })}
+      </div>
     </>
   );
 }
